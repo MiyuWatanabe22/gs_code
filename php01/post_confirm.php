06d5c43d48d6fad7a74c53208db9af5451e2a000
Miyu Watanabe <tcfsmb@gmail.com>
2020-05-28 16:05:07 +0900

    2回目登録

--- php01/post_confirm.php
+++ php01/post_confirm.php
@@ -1,15 +1,22 @@
 <?php
-4flg = 0
+function h($value){
+    return htmlspecialchars($value,ENT_QUOTES);
+}
+
+
+
+
+
+$flg = 0
 $name = $_POST["name"];
 $mail = $_POST["mail"];
-if($name == ""){
-    $name = "未入力です";
-    $flg = 1;
-}
-if($mail == ""){
-    $mail = "未入力です";
-    $flg = 1;
-}
+
+//File書き込み
+//課題は名前とメールの変数をカンマ区切りの文字列にしてファイルに書き込めるようにしてください。
+$file = fopen("data/data.txt","a");	// ファイル読み込み
+fwrite($file, $str."\n");//yamazaki , test@test.jp
+fclose($file);
+//確認のURL（必ずここから）localhost/php01/post.php
 
 
 
@@ -20,8 +27,8 @@ if($mail == ""){
 <title>POST（受信）</title>
 </head>
 <body>
-お名前：<?php echo $name; ?>
-EMAIL：<?php echo $mail; ?>
+お名前：<?php echo h($name); ?>
+EMAIL：<?php echo h($mail); ?>
 <?php if($f1g == 0){
 ?>    
    <button>登録</button>
